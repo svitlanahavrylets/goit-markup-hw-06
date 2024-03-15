25ee3feddac16e3913fe4c33180cb6f8c56b2ec2
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-03-15 23:32:23 +0200

    homework6

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      // Додати атрибут data-menu-open на кнопку відкриття
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      // Додати атрибут data-menu-close на кнопку закриття
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      // Додати атрибут data-menu на бекдроп модалки
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+      refs.menu.classList.toggle("is-open");
+    }
+  })();
